ee7f3e30f478f324f9b4de29d5a03007412321bf
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-11-21 20:28:14 -0500

    fixed this.setstate in postNewTodo

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -22,7 +22,7 @@ export default class App extends React.Component {
   postNewTodo = () => {
     axios.post(URL, { name: this.state.todoNameInput})
       .then(res => {
-        this.fetchAllTodos();
+        this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
         this.resetForm();
       })
       .catch(this.setAxiosResponseError);
@@ -45,7 +45,7 @@ export default class App extends React.Component {
   componentDidMount() {
     this.fetchAllTodos()
   }
-  
+
   render() {
     return (
       <div>
