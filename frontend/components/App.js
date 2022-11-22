d6d06688313aacd5d54361d6bcc64d0f2976e6cf
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-11-21 21:35:00 -0500

    created todolist component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Form from './Form';
+import TodoList from './TodoList';
 
 const URL = 'http://localhost:9000/api/todos'
 
@@ -68,17 +69,11 @@ export default class App extends React.Component {
     return (
       <div>
         <div id="error">Error: {this.state.error}</div>
-        <div id='todos'>
-          <h2>Todos:</h2>
-          {
-            this.state.todos.reduce((acc, todo) => {
-              if (this.state.displayCompleted || !todo.completed) return acc.concat(
-                <div onClick={this.toggleCompleted(todo.id)} key={todo.id}>{todo.name} {todo.completed ? ' ✔️' : ""}</div>
-              );
-              return acc;
-            }, [])
-          }
-        </div>
+        <TodoList 
+          todos={this.state.todos}
+          displayCompleted={this.state.displayCompleted}
+          toggleCompleted={this.toggleCompleted}
+        />
         <Form 
           handleFormSubmit={this.handleFormSubmit}
           handleInputChange={this.handleInputChange}
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default class TodoList extends React.Component {
   render() {
-    return null
+    return (
+      <div id='todos'>
+          <h2>Todos:</h2>
+          {
+            this.props.todos.reduce((acc, todo) => {
+              if (this.props.displayCompleted || !todo.completed) return acc.concat(
+                <div onClick={this.props.toggleCompleted(todo.id)} key={todo.id}>{todo.name} {todo.completed ? ' ✔️' : ""}</div>
+              );
+              return acc;
+            }, [])
+          }
+      </div>
+    )
   }
 }
